3cecc9624dafacd422825be33bed828b335d0adc
Ot7kharlp <kharlphilippe2@gmail.com>
2024-12-23 23:30:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function openGate() {
   // Slide gates to the sides
-  document.getElementById('gate-left').style.transform = 'translateX(100%)'; // Move left gate off-screen
-  document.getElementById('gate-right').style.transform = 'translateX(-100%)'; // Move right gate off-screen
+  document.getElementById('gate-left').style.transform = 'translateX(-100%)'; // Move left gate off-screen
+  document.getElementById('gate-right').style.transform = 'translateX(100%)'; // Move right gate off-screen
 
   // Hide the "Open the Gate" button
   document.querySelector('.open-button').style.display = 'none';
